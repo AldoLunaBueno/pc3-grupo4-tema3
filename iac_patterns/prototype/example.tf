ef255fbd0ea9f3726eb8ecae8a1879fad93bf244
Cinver Esp <cinver.espinoza.v@uni.pe>
2025-06-18 22:39:54 -0500

    feat(module): refinamiento de prototype, correccion de errores con apply

--- iac_patterns/prototype/example.tf
+++ iac_patterns/prototype/example.tf
@@ -1,5 +1,24 @@
-resource "null_resource" "mi_clon"{
-    triggers = {
-        env   = "entorno_basico"
+# Recurso que genera un nombre aleatorio
+resource "random_pet" "mi_clon" {
+  length = 2
+  separator = "-"
+}
+
+# Recurso general nulo
+resource "null_resource" "mi_clon" {
+  triggers = {
+    env = "entorno_basico"
+    pet_id = random_pet.mi_clon.id
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'Creando un clon con nombre mi_clon'"
+  }
+}
+
+output "mi_clon_info"{
+    value = {
+        env = "entorno_basico"
+        pet_name = random_pet.mi_clon.id
     }
 }
--- iac_patterns/prototype/main.tf
+++ iac_patterns/prototype/main.tf
@@ -1,5 +1,31 @@
+# providers para la creación de local_files y templates
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4"
+    }
+  }
+}
 resource "local_file" "prototype_generator" {
-  content  = data.template_file.prototype.rendered
+  content  = data.template_file.prototype_template.rendered
   filename = "${path.module}/example.tf"
 }
 
